46b8bf239a4baf7d7c6ac9f987fc11410b3c2f19
Maxim <sibmaks.khv@mail.ru>
2022-08-09 21:22:13 +1000

    Add Task00

---
+++ Task00/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 0
+
+Console.Write("Введите число: "); // 1. Приглашение к вводу
+// int number = Convert.ToInt32(ConConsole.ReadLine()); // 2. Считывание данных с консоли в тип Int
+string? str = Console.ReadLine(); // 2.1 Считываем данные с консоли
+int number = Convert.ToInt32(str); // 2.2 Конвертируем, полученные данные в число
+Console.Write(number * number); // 3. Вывод на экран число возведенное во 2 степень
